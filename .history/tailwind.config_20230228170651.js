ea16545d1602bad5044c345dbac6ec5c37f12349
Algo Hussle <citizensofandrometta@gmail.com>
2023-02-28 17:19:06 -0600

    %70 complete

---
+++ .history/tailwind.config_20230228170651.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    letterSpacing: {
+      widest: "1em",
+    },
+    extend: {},
+  },
+  plugins: [
+    plugins: [
+      // ...
+      require('tailwind-scrollbar'),
+  
+  ],
+}
